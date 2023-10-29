05f3d63394f2bc2a4f8a23a657789b20f78392d3
Beren Ates <berenatess9918@gmail.com>
2023-10-29 20:38:37 +0300

    Week6 Challange

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
+import Api from "./api/Api";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">STARWARS CHARACTERS</h1>
+      <Api />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,52 @@
-// Karakter bileşeniniz buraya gelecek
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import "./Karakter.css";
+
+const Karakter = ({ character }) => {
+  return (
+    <Accordion>
+      <Accordion.Header>
+        <h1>{character.name}</h1>
+      </Accordion.Header>
+      <Accordion.Body>
+        <p>
+          <b>Karakter Adı:</b> {character.name}
+        </p>
+        <p>
+          <b>gender:</b> {character.gender}
+        </p>
+        <p>
+          <b>Height:</b> {character.height}
+        </p>
+        <p>
+          <b>Mass:</b> {character.mass}
+        </p>
+        <p>
+          <b>BirthYear:</b> {character.birth_year}
+        </p>
+        <p>
+          <b>Eye Color:</b> {character.eye_color}
+        </p>
+        <p>
+          <b>Hair Color:</b> {character.hair_color}
+        </p>
+        <p>
+          <b>Skin Color:</b> {character.skin_color}
+        </p>
+        <Accordion>
+          <Accordion.Header>
+            <b>Films:</b>
+          </Accordion.Header>
+          <Accordion.Body>
+            <ul>
+              {character.films.map((film, index) => (
+                <li key={index}> {film} </li>
+              ))}
+            </ul>
+          </Accordion.Body>
+        </Accordion>
+      </Accordion.Body>
+    </Accordion>
+  );
+};
+export default Karakter;
